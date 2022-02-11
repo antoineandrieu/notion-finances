72b2412afca3b5fde3ca5c0c2b21e9b95ef594c7
Coding Ant <antoine@codingant.io>
2022-02-11 10:19:30 +0100

    fix: notion client secret handling

--- utils/notion.ts
+++ utils/notion.ts
@@ -2,7 +2,7 @@ import { Client } from '@notionhq/client';
 
 export namespace Notion {
   const client = new Client({
-    auth: process.env.NOTION_KEY,
+    auth: process.env.NOTION_SECRET,
   });
   export function getClient() {
     return client;
